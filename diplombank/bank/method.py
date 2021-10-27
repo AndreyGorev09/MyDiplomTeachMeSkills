ddb90e6021f18e99627e05cbc1545f9ff89aa770
AndreiGorev <and.goreff2010@gmail.com>
2021-10-28 00:44:40 +0300

    start: bank

--- diplombank/bank/method.py
+++ diplombank/bank/method.py
@@ -28,8 +28,8 @@ def get_courses():
     eur_sale = raw_eur.get('sale')
     raw_rub = currency_body.find(code='643')
     rub = raw_rub.get('iso')
-    rub_buy = round(float(raw_rub.get('buy')), 3) * 100
-    rub_sale = round(float(raw_rub.get('sale')), 3) * 100
+    rub_buy = round(float(raw_rub.get('buy')), 4) * 100
+    rub_sale = round(float(raw_rub.get('sale')), 4) * 100
     result.append(CoursesBank(
         data=data,
         usd=usd,
